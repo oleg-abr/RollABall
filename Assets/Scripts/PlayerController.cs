96fd0a32c3446692862db853b77b4aa14e76fd4a
Oleg Abrashchov <oleg.abr@gmail.com>
2017-02-01 19:13:03 +0100

    initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+  public class PlayerController : MonoBehaviour
+  {
+    private Rigidbody rb;
+
+    public float speed;
+    void Start()
+    {
+      rb = GetComponent<Rigidbody>();
+    }
+    void FixedUpdate()
+    {
+      float moveHorizontal = Input.GetAxis("Horizontal");
+      float moveVertical = Input.GetAxis("Vertical");
+      var movement = new Vector3(moveHorizontal, 0f, moveVertical);
+      rb.AddForce(movement * speed);
+    }
+  }
+}
